bd9d79b3880e24490d085b6f3fa5369c8d9ca41b
NahuelIroz <nahuel.t.p@hotmail.com>
2026-05-17 19:39:21 -0300

    feat(api): modificar packages/api/src/application/UpdatePaymentUseCase.ts

--- packages/api/src/application/UpdatePaymentUseCase.ts
+++ packages/api/src/application/UpdatePaymentUseCase.ts
@@ -3,7 +3,7 @@ import { PaymentRepository } from '../domain/PaymentRepository.js';
 export interface UpdatePaymentInput {
   id: string;
   amount?: number;
-  status?: 'Pending' | 'Paid' | 'Canceled' | 'Ovedue';
+  status?: 'Pending' | 'Paid' | 'Canceled' | 'Ovedue'; 
   dueDate?: Date;
 }
 
@@ -13,15 +13,31 @@ export class UpdatePaymentUseCase {
   ) {}
 
   async execute(input: UpdatePaymentInput) {
+    
     const existingPayment = await this.paymentRepository.findById(input.id);
+    
+    
     if (!existingPayment) {
-      throw new Error("El registro de pago no existe.");
+      throw new Error("NOT_FOUND"); 
     }
 
-    return await this.paymentRepository.update(input.id, {
+    if (existingPayment.status === 'Paid') {
+      throw new Error("ALREADY_PAID"); 
+    }
+    if (existingPayment.status === 'Canceled') {
+      throw new Error("IS_CANCELED"); 
+    }
+
+    const updatedData: any = {
       amount: input.amount ?? existingPayment.amount,
       status: input.status ?? existingPayment.status,
       dueDate: input.dueDate ?? existingPayment.dueDate,
-    });
+    };
+
+    if (input.status === 'Paid') {
+      updatedData.paymentDate = new Date(); 
+    }
+
+    return await this.paymentRepository.update(input.id, updatedData);
   }
 }
