9a7738b8ac4575a664776c6d8516186dbb1f8a4f
Fermín Etchanchú Paoltroni <etchanchufermin_1992@hotmail.com>
2026-05-27 19:23:02 -0300

    feat(sport): agrega test e2e alta sport y archivos de configuración

---
+++ packages/api/src/delivery/Sport.e2e.test.ts
@@ -0,0 +1,60 @@
+import 'dotenv/config';
+import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import { FastifyInstance } from 'fastify';
+import { buildApp } from '../app.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/client/client.js';
+
+describe('Sport API End-to-End Tests', () => {
+    let app: FastifyInstance;
+    let prisma: PrismaClient;
+    let createdSportId: string | undefined;
+
+    const randomSuffix = Math.floor(Math.random() * 100000).toString();
+    const testSportName = `Deporte E2E ${randomSuffix}`;
+
+    beforeAll(async () => {
+        app = buildApp();
+        await app.ready();
+        
+        prisma = new PrismaClient({
+            adapter: new PrismaPg(process.env.DATABASE_URL as any)
+        });
+        await prisma.$connect()
+    });
+
+    afterAll(async () => {
+        if (createdSportId) {
+            await prisma.sport.deleteMany({
+                where: { id: createdSportId }
+            });
+        }
+        await prisma.$disconnect();
+        await app.close();
+    });
+
+    it('1. POST: Debe crear un deporte en la base de datos real', async () => {
+        const payload ={
+            name: testSportName,
+            description: 'Descripción E2E',
+            max_capacity: 10,
+            additional_price: 0,
+            requires_medical_certificate: true
+        };
+
+        const response = await app.inject({
+            method: 'POST',
+            url: '/api/v1/sports',
+            payload
+        });
+
+        expect(response.statusCode).toBe(201);
+        const body = JSON.parse(response.payload);
+        expect(body.data.id).toBeDefined();
+        expect(body.data.name).toBe(testSportName);
+        createdSportId = body.data.id;
+        const dbSport = await prisma.sport.findUnique({ where: { id: createdSportId}});
+        expect(dbSport).not.toBeNull();
+        expect(dbSport?.name).toBe(testSportName);
+    });
+})
